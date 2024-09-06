32e8ec88869116bdbeb95b8e5f3dcf7c2696a9d5
Nikola Arlotti <aksamitna.egzystencja@gmail.com>
2024-09-06 22:40:46 +0200

    fix error

--- config-general.nix
+++ config-general.nix
@@ -12,7 +12,7 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.device = nodev;
   boot.loader.grub.useOSProber = true;
 
   # Enable networking
@@ -38,6 +38,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
