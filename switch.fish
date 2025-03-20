0bf5dcbff7da0948f76f4f59656a557db4e6e138
Nikola Arlotti <aksamitna.egzystencja@gmail.com>
2025-03-21 00:07:19 +0100

    checkpoint 85 (update)

--- switch.fish
+++ switch.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-git commit -am "checkpoint $(git rev-list --count HEAD)"
+git commit -am "checkpoint $(git rev-list --count HEAD) (switch)"
 git push
 sudo nixos-rebuild switch --flake . &| nom
--- update.fish
+++ update.fish
@@ -2,6 +2,6 @@
 
 sudo nix flake update &&
     sudo nixos-rebuild build --flake . &&
-    git commit -am "checkpoint $(git rev-list --count HEAD)" &&
+    git commit -am "checkpoint $(git rev-list --count HEAD) (update)" &&
     git push &&
     sudo nixos-rebuild switch --flake . &| nom
